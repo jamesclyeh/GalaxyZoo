5e5ea20c97ef812ff841449b07e7908d0221420f
abdul-qureshi <abdulhqureshi@gmail.com>
2014-03-08 02:06:03 -0500

    Modified Galaxy.py to accept new batch size

--- Galaxy.py
+++ Galaxy.py
@@ -6,26 +6,27 @@ class Galaxy(dense_design_matrix.DenseDesignMatrix):
 
     def __init__(self,which_set = 'train', axes=('b', 0, 1, 'c'), preprocessor = None):
         dtype = 'uint8'
-        ntrain = 60000
-        nvalidation = 1578
+        batch_size = 10263
+        ntrain = 5*batch_size
+        nvalidation = batch_size
         self.img_shape = (3, 100, 100)
         self.n_classes = 37
         self.axes = axes;
         self.img_size = N.prod(self.img_shape)
-        
-        fdata_names = ['galaxy_batch_%i.npy' % i for i in range(0,7)]
-        flabels_names = ['galaxy_batch_solution_%i.npy' % i for i in range(0,7)]
-        lenx = N.ceil((ntrain) / 10000.)*10000
-        x = N.zeros((lenx,self.img_size), dtype=dtype)
-        y = N.zeros((lenx, self.n_classes), dtype=dtype)
+ 
+        fdata_names = ['galaxy_batch_%i.npy' % i for i in range(0,6)]
+        flabels_names = ['galaxy_batch_solution_%i.npy' % i for i in range(0,6)]
+
+        x = N.zeros((batch_size*6,self.img_size), dtype=dtype)
+        y = N.zeros((batch_size*6, self.n_classes), dtype=dtype)
         
         nloaded = 0
         for i in xrange(len(fdata_names)):
             data = N.load(fdata_names[i])
             labels = N.load(flabels_names[i]) 
-            x[i*10000:(i+1)*10000, :] = data 
-            y[i*10000:(i+1)*10000] = labels
-            nloaded += len(data)
+            x[i*batch_size:(i+1)*batch_size] = data 
+            y[i*batch_size:(i+1)*batch_size] = labels
+            nloaded += batch_size
             #if nloaded >= ntrain + nvalidation : break;
         
         Xs = {
@@ -44,8 +45,7 @@ class Galaxy(dense_design_matrix.DenseDesignMatrix):
             y = N.asarray(y)
             
         if which_set == 'validation':
-            print y
-            assert y.shape[0] == 1578
+            assert y.shape[0] == batch_size
         
         view_converter = dense_design_matrix.DefaultViewConverter((100, 100, 3), axes)
         super(Galaxy, self).__init__(X=X, y=y, view_converter=view_converter)
